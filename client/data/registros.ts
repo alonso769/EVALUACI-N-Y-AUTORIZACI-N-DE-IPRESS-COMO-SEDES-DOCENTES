7e9c298057e75a6834a144341fde72d9845d0eda
alonso769 <131563220+alonso769@users.noreply.github.com>
2025-10-03 10:01:46 -0500

    CAMB

--- client/data/registros.ts
+++ client/data/registros.ts
@@ -10,55 +10,55 @@ const registros: Registro[] = [
     ris: "RIS 1",
     establecimiento: "CS SAN SEBASTIAN",
     jefatura: "MC. MANUEL ANGEL MATEO YAURI",
-    enlace: "https://drive.google.com/drive/folders/1UiPiTiBkQmWVyakkDvBq8-1p9EQ02usp-sharing"
+    enlace: "https://drive.google.com/drive/folders/1F_UjzjTiBKpUeWyakAzDkVa83x8lh9EO?usp=sharing"
   },
   {
     ris: "RIS 1",
     establecimiento: "CS UNIDAD VECINAL N°3",
     jefatura: "MC. NATALI EMPERATRIZ VENANCIO ESPINOZA",
-    enlace: "https://drive.google.com/drive/folders/1xIleScRPyV6ODGzlWfnpV8r_uw0mNG1J2p-sharing"
+    enlace: "https://drive.google.com/drive/folders/1xUeAcrPYV6ODGXIwFpW8t_uwQzMNGJ9i?usp=sharing"
   },
   {
     ris: "RIS 1",
     establecimiento: "CS CONDE DE LA VEGA",
     jefatura: "MC. MARIZA CAPCHA ROJAS",
-    enlace: "https://drive.google.com/drive/folders/1bDby0Crb1vLz7Yl2pRqNFbqf-0zsharing"
+    enlace: "https://drive.google.com/drive/folders/1hDDu08cpIbV1RGAFNL7DK8ofXxIZ1v-a?usp=sharing"
   },
   {
     ris: "RIS 2",
     establecimiento: "CSMI MAGDALENA",
     jefatura: "MC. MARIA KATYA URIBE DE LA TORRE",
-    enlace: "https://drive.google.com/drive/folders/1qjW7NxN3Pths7YMhrHN1-L7Nqw2p-sharing"
+    enlace: "https://drive.google.com/drive/folders/1q7jWzXN33PhtL7IpMnrflfNJ-37NK0fu?usp=sharing"
   },
   {
     ris: "RIS 2",
     establecimiento: "CS BREÑA",
     jefatura: "MC. DARIO FLAVIO RODRIGUEZ RAMIREZ",
-    enlace: "https://drive.google.com/drive/folders/1eF8Wv3uMfDngWLIyP1sCmQzW2vU-sharing"
+    enlace: "https://drive.google.com/drive/folders/1fetBW3uMfdnBWLjH9LyFjtsCmCzaezhW?usp=sharing"
   },
   {
     ris: "RIS 2",
     establecimiento: "CS JESUS MARIA",
     jefatura: "MC. ROCIO JIMENEZ CAMANA",
-    enlace: "https://drive.google.com/drive/folders/1jpRoxaDrDqMf67S1eVBKbdb2Qp-sharing"
+    enlace: "https://drive.google.com/drive/folders/1plJrgqxaDRq4Mfqr675JzFVB_jGbbd4Q?usp=sharing"
   },
   {
     ris: "RIS 2",
     establecimiento: "CS SAN MIGUEL",
     jefatura: "MC. ANA ERIKA CHINEN YARA",
-    enlace: "https://drive.google.com/drive/folders/1QheFWXRkhops7L3kC7tCx8-yL3x2p-sharing"
+    enlace: "https://drive.google.com/drive/folders/1QhgEWXrIkohzp7LsBQgCJTc4Sh_ydL3x?usp=sharing"
   },
   {
     ris: "RIS 3",
     establecimiento: "CSMI SURQUILLO",
     jefatura: "MC. MARGARITA SUSANA SILVA DÁVILA",
-    enlace: "https://drive.google.com/drive/folders/1QMMW5SyYpaiUGe2p6GJxY7mpLQG1Yp-sharing"
+    enlace: "https://drive.google.com/drive/folders/1QMWW55yYpjlUQgFvSp6x_G1gV7mpl5yK?usp=sharing"
   },
   {
     ris: "RIS 3",
     establecimiento: "CS VILLA VICTORIA PORVENIR",
     jefatura: "MC. ROSA VICENTA RODRIGUEZ GARCIA",
-    enlace: "https://drive.google.com/drive/folders/1fL2zVfrmHbXYvkUjlBwq9t1Wp12p-sharing"
+    enlace: "https://drive.google.com/drive/folders/1pIj52YvmF8hXYzsshrZ65lokYLOGd1yP?usp=sharing"
   },
   {
     ris: "RIS 3",
@@ -70,79 +70,85 @@ const registros: Registro[] = [
     ris: "RIS 3",
     establecimiento: "CS SAN ISIDRO",
     jefatura: "MC. MARIA DEL CARMEN BERTRAMINI",
-    enlace: "https://drive.google.com/drive/folders/1IpdL1qU1MICvklyuBwq9t1Wp2p-sharing"
+    enlace: "https://drive.google.com/drive/folders/1nllI4lb_jpJiMjCyeXvUq-L8w91cLNWJ?usp=sharing"
   },
   {
     ris: "RIS 3",
     establecimiento: "CS SANTA CRUZ DE MIRAFLORES",
     jefatura: "MC. JHONNY ISRAEL MAMANI VERA",
-    enlace: "https://drive.google.com/drive/folders/1kpQldDpqeZ6pT1YLmktwk6Up-sharing"
+    enlace: "https://drive.google.com/drive/folders/12Kp0IdDPpQ-geE2dit1TTSLVmrKwuzrn?usp=sharing"
+  },
+  {
+    ris: "RIS 4",
+    establecimiento: "CS SAN COSME",
+    jefatura: "MC. LUIS REYNALDO EXEBIO MONTOYA",
+    enlace: "https://drive.google.com/drive/folders/18o9bwISxS9LVpegrSQySsA8JUXaIwlJ4?usp=sharing"
   },
   {
     ris: "RIS 4",
     establecimiento: "CSMC SAN BORJA",
     jefatura: "MC. JOSUE ALBERTO VASQUEZ MEDINA",
-    enlace: "https://drive.google.com/drive/folders/1eA6s49AqZ02xtbN1QdA7YN3p-sharing"
+    enlace: "https://drive.google.com/drive/folders/1gbAes9AOeZ0PxtbmvTcOJneAY7Nx3fgf?usp=sharing"
   },
   {
     ris: "RIS 4",
     establecimiento: "CSMC SAN COSME",
     jefatura: "MC. VALERIA VALENZUELA RICCIO",
-    enlace: "https://drive.google.com/drive/folders/10geQ9v7u8n8sG0KupHnS1xA-e2702p-sharing"
+    enlace: "https://drive.google.com/drive/folders/10oae0pY9uBn860lXuPXlm5irA-xe27OZ?usp=sharing"
   },
   {
     ris: "RIS 4",
     establecimiento: "CSMI EL PORVENIR",
     jefatura: "MC. LUIS ALBERTO MOLOCHO ARANGO",
-    enlace: "https://drive.google.com/drive/folders/1ARFPNRdRzL8W7Z3u6m2ENHASCp-sharing"
+    enlace: "https://drive.google.com/drive/folders/1R2FFNRFtazlUr8W723jU6a23mVEhA9C1?usp=sharing"
   },
   {
     ris: "RIS 4",
     establecimiento: "CS SAN BORJA",
     jefatura: "MC. DIANA CAROLINA DEL ARCA MURGA",
-    enlace: "https://drive.google.com/drive/folders/1SS9-hnOchfCfHl1FaqYatVl1zp-sharing"
+    enlace: "https://drive.google.com/drive/folders/1S5-hnOcdfbCnfHLJHLfagYqleyATyit2?usp=sharing"
   },
   {
     ris: "RIS 5",
     establecimiento: "CS CAMPOY",
     jefatura: "MC. MARIA LUZ VARGAS RAMOS",
-    enlace: "https://drive.google.com/drive/folders/1ownVG9SLuEvw7zpJhpk9S89m-2p-sharing"
+    enlace: "https://drive.google.com/drive/folders/1qnVF9LGI5luzEw7zzbA_Jhkp9F389m-2?usp=sharing"
   },
   {
     ris: "RIS 6",
     establecimiento: "CS HUASCAR XV",
     jefatura: "MC. IRMA VIOLETA HIDALGO VEGA",
-    enlace: "https://drive.google.com/drive/folders/1CMhMpFzau52RXm8XDdT5pJDYp-sharing"
+    enlace: "https://drive.google.com/drive/folders/1JCMRaqHFZaxJ52RGXXnzBXXd0T5pJDPY?usp=sharing"
   },
   {
     ris: "RIS 6",
     establecimiento: "CS HUASCAR II",
     jefatura: "MC. MARIA ISABEL OLIVOS HERRERA",
-    enlace: "https://drive.google.com/drive/folders/1UGpTED1XkpTfDUXp-sharing"
+    enlace: "https://drive.google.com/drive/folders/14F6xAkvdUO0lV3JNSUoiV6LKPeTiEDUX?usp=sharing"
   },
   {
     ris: "RIS 6",
     establecimiento: "CS GAMINEDES",
     jefatura: "MC. IVAN ALFREDO CAMPOSANO GUERRERO",
-    enlace: "https://drive.google.com/drive/folders/1UGpTES1x8lsGzAkyR8BBH-Fp-sharing"
+    enlace: "https://drive.google.com/drive/folders/1UPGiTEbS18xIgWPBl_aIZsAY3R8BKH-F?usp=sharing"
   },
   {
     ris: "RIS 6",
     establecimiento: "CS BAYOVAR",
     jefatura: "MC. YESSICA KARIN RAFAEL TERREROS",
-    enlace: "https://drive.google.com/drive/folders/1zHcDw1C7ZlVRHtg-sharing"
+    enlace: "https://drive.google.com/drive/folders/1ZhCdw-IA59phRegBnsgJCf1ZZLvtRvFh?usp=sharing"
   },
   {
     ris: "RIS 7",
     establecimiento: "CS CRUZ DE MOTUPE",
     jefatura: "MC. DARWIN MICHELL QUISPE BALBIN",
-    enlace: "https://drive.google.com/drive/folders/1u0mKtNQx8laNAtoCfriupzYL3p-sharing"
+    enlace: "https://drive.google.com/drive/folders/1UumfKL_qX8IANAtocpYr2rjjJp2v2LY3?usp=sharing"
   },
   {
     ris: "RIS 7",
     establecimiento: "CS 10 DE OCTUBRE",
     jefatura: "MC. DARIO FLAVIO RODRIGUEZ RAMIREZ",
-    enlace: "https://drive.google.com/drive/folders/1Oj7X6fMxKq1v8BdgT1BYLvS1xp-sharing"
+    enlace: "https://drive.google.com/drive/folders/1I0ZfJXfdS6MkX1v8Dl8jt0TB1Y7MS1Kf?usp=sharing"
   }
 ];
 
